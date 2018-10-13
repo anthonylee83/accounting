b2106b070345ec984384a2155f7cbc9918b7bca3
wickygilb <wgilberspsu@gmail.com>
2018-10-13 15:05:51 -0400

    Modified the linked directory the stylesheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('resources/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">

622b135540f24e6b88f3ffce0a27646150e88354
Patrick Hughes <smilypie1989@gmail.com>
2018-10-21 15:11:34 -0400

    Added more headings in journal display

--- resources/views/journalize/index.blade.php
+++ resources/views/journalize/index.blade.php
@@ -5,18 +5,47 @@
 
     <journalizer :accounts="{{json_encode($accounts)}}"></journalizer>
 
+    <!--
+    <div class="text-right -mb-4">
+        <a dusk="newJournal" href="{{action('Admin\UserController@newUser')}}" class="btn btn-lg btn-info btn-circle text-right" role="button"><i class="fas fa-plus text-white"></i></a>
+    </div>
+    -->
+
     <table class="table table-striped table-hover">
         <thead>
             <th>Reference</th>
+            <th>Description</th>
+            <th>Accounts</th>
+            <th>Amounts</th>
             <th>Approved</th>
-            <th>Created_at</th>
+            <th>Created At</th>
+            @if( Auth::user()->profile->access_level_id == 2)
+                <th>Controls</th>
+            @endif
         </thead>
         <tbody>
     @forelse($entries as $je)
         <tr>
             <td>{{$je->reference}}</td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td> {{ $je->approved ? 'Approved' : 'Pending'}}</td>
             <td>{{$je->created_at}}</td>
+            @if( Auth::user()->profile->access_level_id == 2)
+                <td>
+                    <div>
+                        <ul class="navbar-nav ml-auto">
+                            <li>
+                                <a class="btn btn-primary">Approve</a>
+                            </li>
+                            <li>
+                                <a class="btn btn-danger">Reject</a>
+                            </li>
+                        </ul>
+                    </div>
+                </td>
+            @endif
         </tr>
     @empty
         <tr><td cols=4>No journal entries</td></tr>

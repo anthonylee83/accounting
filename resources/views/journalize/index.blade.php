4e6c76d491365da12facb99bf95d3f92f53b9a43
Patrick Hughes <smilypie1989@gmail.com>
2018-10-28 18:57:46 -0400

    Added 'Approved By' column in journal, but it appears empty unless the entry isn't approved

--- resources/views/journalize/index.blade.php
+++ resources/views/journalize/index.blade.php
@@ -5,12 +5,6 @@
 
     <journalizer :accounts="{{json_encode($accounts)}}"></journalizer>
 
-    <!--
-    <div class="text-right -mb-4">
-        <a dusk="newJournal" href="{{action('Admin\UserController@newUser')}}" class="btn btn-lg btn-info btn-circle text-right" role="button"><i class="fas fa-plus text-white"></i></a>
-    </div>
-    -->
-
     <table class="table table-striped table-hover">
         <thead>
             <th>Date</th>
@@ -19,8 +13,9 @@
             <th>Debit</th>
             <th>Credit</th>
             <th>Approved</th>
+            <th>Approved By</th>
             @if( Auth::user()->profile->access_level_id == 2)
-                <th>Controls</th>
+                <th></th>
             @endif
         </thead>
         <tbody>
@@ -45,12 +40,18 @@
             <td>{{ $je->debit ?? $je->amount }}</td>
             <td>{{ !$je->debit ?? $je->amount }}</td>
             <td> {{ $je->approved ? 'Approved' : 'Pending'}}</td>
-            <td>{{$je->created_at}}</td>
+
+            @if( $je->approved == true)
+                <td>{{$je->user['name']}}</td>
+            @else
+                <td>N/A</td>
+            @endif
+
             @if( Auth::user()->profile->access_level_id == 2)
                 <td>
                     <div>
                         <ul class="navbar-nav ml-auto">
-                        <a 
+                        <a
                             href="{{action('JournalController@approve', $je->id)}}"
                             class="btn btn-primary">Approve</a>
                         <a

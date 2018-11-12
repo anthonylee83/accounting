37073b855dba0a647a0fab77e2d6c5e481b6be8e
Anthony Lee <anthonylee83@gmail.com>
2018-11-11 20:06:16 -0500

    Removed descriptions from transactions and placed on Journal Entries

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -9,6 +9,7 @@ use App\Transaction;
 use App\AccountNormalSide;
 use Auth;
 use App\EventLog;
+use App\Attachment;
 
 class JournalController extends Controller
 {
@@ -47,16 +48,26 @@ class JournalController extends Controller
             'created_user_id'      => Auth::user()->id,
             'document_reference_id'=> 0,
             'reference'            => rand(10000, 999999),
-            'approval_user_id'     => 0
+            'approval_user_id'     => 0,
+            'description'          => $request->description ?? ''
         ]);
 
-        //return $request->transactions;
+        if ($request->hasFile('attachments')) {
+            foreach ($request->file('attachments') as $attachment) {
+                return response()->json($attachment);
+                $file = $attachment->store('attachments');
+                Attachment::create(['journal_entry_id'  => $journal->id,
+                                    'file'              => $file]);
+            };
+        } else {
+            abort(500, 'No Files');
+        }
+
         $transactions = [];
-        foreach ($request->transactions as $transaction) {
+        foreach (json_decode($request->transactions, true) as $transaction) {
             //return response()->json($transaction);
             $t              = new Transaction();
             $t->account_id  = $transaction['account_id'];
-            $t->description = $transaction['description'] == null ? '' : $transaction['description'];
             if ($transaction['debit'] > 0) {
                 $t->debit  = true;
                 $t->amount = $transaction['debit'];
@@ -67,12 +78,12 @@ class JournalController extends Controller
 
             $journal->transactions()->save($t);
         }
-		EventLog::create([
-		'email'       =>  session('email'),
-		'action' => "Journalized a new transaction"
-		]);
+        EventLog::create([
+        'email'       => session('email'),
+        'action'      => 'Journalized a new transaction'
+        ]);
 
-        return response()->json($journal);
+        return redirect()->action('JournalController@index');
     }
 
     /**
@@ -123,41 +134,34 @@ class JournalController extends Controller
     public function approve($id)
     {
         $entry           = JournalEntry::findOrFail($id);
-        $transactions = Transaction::where('journal_entry_id', $id)->get();
+        $transactions    = Transaction::where('journal_entry_id', $id)->get();
 
         // Amount validation
-        foreach($transactions as $transaction)
-        {
-            $accountID = $transaction->account_id;
-            $account = Account::find($accountID);
-            $normal = AccountNormalSide::find($account->account_normal_side_id);
-            $accountBalance = preg_replace("/[^0-9.]/", "", "$account->account_balance");
-            $amount = $transaction->amount;
+        foreach ($transactions as $transaction) {
+            $accountID      = $transaction->account_id;
+            $account        = Account::find($accountID);
+            $normal         = AccountNormalSide::find($account->account_normal_side_id);
+            $accountBalance = preg_replace('/[^0-9.]/', '', "$account->account_balance");
+            $amount         = $transaction->amount;
 
-            if ($transaction->debit != $normal->journal_binary)
-            {
-                if ($accountBalance < $amount)
-                {
+            if ($transaction->debit != $normal->journal_binary) {
+                if ($accountBalance < $amount) {
                     return redirect()->action('ApprovalController@index');
                 }
             }
         }
 
-        foreach($transactions as $transaction)
-        {
-            $accountID = $transaction->account_id;
-            $account = Account::find($accountID);
-            $normal = AccountNormalSide::find($account->account_normal_side_id);
-            $accountBalance = preg_replace("/[^0-9.]/", "", "$account->account_balance");
-            $amount = $transaction->amount;
+        foreach ($transactions as $transaction) {
+            $accountID      = $transaction->account_id;
+            $account        = Account::find($accountID);
+            $normal         = AccountNormalSide::find($account->account_normal_side_id);
+            $accountBalance = preg_replace('/[^0-9.]/', '', "$account->account_balance");
+            $amount         = $transaction->amount;
 
-            if ($transaction->debit == $normal->journal_binary)
-            {
-                    $accountBalance = $accountBalance + $amount;
-            }
-            elseif ($transaction->debit != $normal->journal_binary)
-            {
-                    $accountBalance = $accountBalance - $amount;
+            if ($transaction->debit == $normal->journal_binary) {
+                $accountBalance = $accountBalance + $amount;
+            } elseif ($transaction->debit != $normal->journal_binary) {
+                $accountBalance = $accountBalance - $amount;
             }
             $account->account_balance = $accountBalance;
             $account->save();
@@ -190,12 +194,10 @@ class JournalController extends Controller
             $account->save();
 
         } */
-		EventLog::create([
-		'email'       =>  session('email'),
-		'action' => "Approved journal entry: {$id}"
-		]);
-
-
+        EventLog::create([
+        'email'       => session('email'),
+        'action'      => "Approved journal entry: {$id}"
+        ]);
 
         return redirect()->action('ApprovalController@index');
     }
@@ -203,10 +205,10 @@ class JournalController extends Controller
     public function decline($id)
     {
         $entry = JournalEntry::findOrFail($id);
-		EventLog::create([
-		'email'       =>  session('email'),
-		'action' => "Declined journal entry: {$id}"
-		]);
+        EventLog::create([
+        'email'       => session('email'),
+        'action'      => "Declined journal entry: {$id}"
+        ]);
         $entry->delete();
         return redirect()->action('ApprovalController@index');
     }
--- resources/assets/js/transaction.js
+++ resources/assets/js/transaction.js
@@ -3,14 +3,12 @@ export default class Transaction{
         if(transaction !== undefined){
             this.id = transaction.id;
             this.account_id = transaction.account_id;
-            this.description = transaction.description;
             this.credit = transaction.credit;
             this.debit = transaction.debit;
 
         }else{
             this.id= null,
             this.account_id= null,
-            this.description= null,
             this.credit= null,
             this.debit= null
         }

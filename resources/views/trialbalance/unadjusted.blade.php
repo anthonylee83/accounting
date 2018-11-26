e100984789a6a2a620667ba26a6a273ea1281dd4
jluttrell117 <42498094+jluttrell117@users.noreply.github.com>
2018-11-25 21:25:31 -0500

    trial balance date picker position moved

--- resources/views/trialbalance/unadjusted.blade.php
+++ resources/views/trialbalance/unadjusted.blade.php
@@ -3,7 +3,7 @@
 
 
 @section('content')
-<div class="float-right picker-wrap">
+<div class="float-right picker-wrap" style=" position: fixed; right: 50px">
             As of
         <input class="date-picker-unadjusted" type="date" name="date" value="{{$date->format('Y-m-d')}}">
         <a id="date-setter-unadjusted" href="/trial/unadjusted">Update</a>
